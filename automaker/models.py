2fd6d5c9765e9875ff4b1655138cbb854572ce48
cruerlin <cruerlin12@gmail.com>
2024-04-19 10:40:09 +0600

    добавление моделей

--- automaker/models.py
+++ automaker/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class Client(models.Model):
+    full_name = models.CharField(max_length=100)
+    address = models.CharField(max_length=200)
+    phone = models.CharField(max_length=20)
+    email = models.EmailField()
+
+class Master(models.Model):
+    name = models.CharField(max_length=100)
+    experience = models.IntegerField()
+    certification = models.BooleanField(default=False)
+    specialization = models.CharField(max_length=100)
+
+class Service(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+
+class Appointment(models.Model):
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)
+    master = models.ForeignKey(Master, on_delete=models.CASCADE)
+    services = models.ManyToManyField(Service)
+    total_cost = models.DecimalField(max_digits=8, decimal_places=2)
+
